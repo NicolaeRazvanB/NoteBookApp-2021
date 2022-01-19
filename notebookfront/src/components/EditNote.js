88aec781143f94c8dd1645a429cc653e0909bba7
Andrada Budui <andrada.budui@yahoo.com>
2022-01-19 21:36:00 +0200

    added styling to existing components

--- notebookfront/src/components/EditNote.js
+++ notebookfront/src/components/EditNote.js
@@ -1,77 +1,97 @@
 import { React, useState, useEffect } from "react";
 import axios from "axios";
+import "../styles/EditNote.css";
 export default function EditNote({ note, curentUser }) {
-  const [editedNote, setEditedNote] = useState(note);
-  const [isSaved, setIsSaved] = useState(false);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(editedNote);
-    axios
-      .put("/api/users/" + curentUser.id + "/notes/" + note.id, editedNote)
-      .then((response) => {
-        console.log(response.status);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-    setIsSaved(true);
-  };
-  if (isSaved === false)
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>Title</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue={note.name}
-            placeholder="Title of your note"
-            onChange={(e) => (editedNote.name = e.target.value)}
-          />
-          <label>Subject</label>
-          <input
-            type="text"
-            name="subject"
-            defaultValue={note.subject}
-            placeholder="Subject of the note"
-            onChange={(e) => (editedNote.subject = e.target.value)}
-          />
-          <label>Activity Type</label>
-          <input
-            type="text"
-            name="activityType"
-            defaultValue={note.activityType}
-            placeholder="Activity Type"
-            onChange={(e) => (editedNote.activityType = e.target.value)}
-          />
-          <label>Activity Number</label>
-          <input
-            type="text"
-            name="activityNumber"
-            defaultValue={note.activityNumber}
-            placeholder="Activity Number"
-            onChange={(e) => (editedNote.activityNumber = e.target.value)}
-          />
-          <label>Activity Date</label>
-          <input
-            type="text"
-            name="activityDate"
-            defaultValue={note.activityDate}
-            placeholder="Activity Date"
-            onChange={(e) => (editedNote.activityDate = e.target.value)}
-          />
-          <br />
-          <textarea
-            rows="25"
-            cols="100"
-            defaultValue={note.text}
-            placeholder="Start writing your note "
-            onChange={(e) => (editedNote.text = e.target.value)}
-          ></textarea>
-          <br />
-          <input type="submit" value="Save Changes" />
-        </form>
-      </div>
-    );
-  else return <h1>Changes saved succesfully</h1>;
+    const [editedNote, setEditedNote] = useState(note);
+    const [isSaved, setIsSaved] = useState(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(editedNote);
+        axios
+            .put(
+                "/api/users/" + curentUser.id + "/notes/" + note.id,
+                editedNote
+            )
+            .then((response) => {
+                console.log(response.status);
+            })
+            .catch((error) => {
+                console.log(error.response);
+            });
+        setIsSaved(true);
+    };
+    if (isSaved === false)
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-content">
+                        <div className="inputs">
+                            <label>Title</label>
+                            <input
+                                type="text"
+                                name="name"
+                                defaultValue={note.name}
+                                placeholder="Title of your note"
+                                onChange={(e) =>
+                                    (editedNote.name = e.target.value)
+                                }
+                            />
+                            <label>Subject</label>
+                            <input
+                                type="text"
+                                name="subject"
+                                defaultValue={note.subject}
+                                placeholder="Subject of the note"
+                                onChange={(e) =>
+                                    (editedNote.subject = e.target.value)
+                                }
+                            />
+                            <label>Activity Type</label>
+                            <input
+                                type="text"
+                                name="activityType"
+                                defaultValue={note.activityType}
+                                placeholder="Activity Type"
+                                onChange={(e) =>
+                                    (editedNote.activityType = e.target.value)
+                                }
+                            />
+                            <label>Activity Number</label>
+                            <input
+                                type="text"
+                                name="activityNumber"
+                                defaultValue={note.activityNumber}
+                                placeholder="Activity Number"
+                                onChange={(e) =>
+                                    (editedNote.activityNumber = e.target.value)
+                                }
+                            />
+                            <label>Activity Date</label>
+                            <input
+                                type="text"
+                                name="activityDate"
+                                defaultValue={note.activityDate}
+                                placeholder="Activity Date"
+                                onChange={(e) =>
+                                    (editedNote.activityDate = e.target.value)
+                                }
+                            />
+                        </div>
+                        <textarea
+                            rows="25"
+                            cols="100"
+                            defaultValue={note.text}
+                            placeholder="Start writing your note "
+                            onChange={(e) => (editedNote.text = e.target.value)}
+                        ></textarea>
+                    </div>
+                    <input
+                        className="save-button"
+                        type="submit"
+                        value="Save Changes"
+                    />
+                </form>
+            </div>
+        );
+    else return <h1>Changes saved succesfully</h1>;
 }
--- notebookfront/src/components/ViewNotes.js
+++ notebookfront/src/components/ViewNotes.js
@@ -1,31 +1,36 @@
 import { React, useState, useEffect } from "react";
 import NoteCard from "./NoteCard";
 import axios from "axios";
+import "../styles/ViewNotes.css";
 export default function ViewNotes({ curentUser }) {
-  const [notes, setNotes] = useState([]);
+    const [notes, setNotes] = useState([]);
 
-  useEffect(() => {
-    const FetchData = async () => {
-      try {
-        const resp = await axios.get("/api/notes/" + curentUser.id + "/notes");
-        console.log(resp.data);
-        setNotes(resp.data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    FetchData();
-  }, [notes]);
+    useEffect(() => {
+        const FetchData = async () => {
+            try {
+                const resp = await axios.get(
+                    "/api/notes/" + curentUser.id + "/notes"
+                );
+                console.log(resp.data);
+                setNotes(resp.data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        FetchData();
+    }, [notes]);
 
-  return (
-    <div>
-      {notes.length === 0 && <h1>You currently haven't added any notes</h1>}
-      {notes.length > 0 &&
-        notes.map((note) => (
-          <div key={note.id}>
-            <NoteCard note={note} curentUser={curentUser} />
-          </div>
-        ))}
-    </div>
-  );
+    return (
+        <div className="notes-container">
+            {notes.length === 0 && (
+                <h1>You currently haven't added any notes</h1>
+            )}
+            {notes.length > 0 &&
+                notes.map((note) => (
+                    <div key={note.id}>
+                        <NoteCard note={note} curentUser={curentUser} />
+                    </div>
+                ))}
+        </div>
+    );
 }
