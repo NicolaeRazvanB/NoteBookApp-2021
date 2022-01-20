898cb5acf69a648151065a12296162484f56df22
NicolaeRazvanB <nicolassky16@gmail.com>
2022-01-20 03:45:29 +0200

    finishing touches

--- notebookfront/src/components/ViewNotes.js
+++ notebookfront/src/components/ViewNotes.js
@@ -2,35 +2,32 @@ import { React, useState, useEffect } from "react";
 import NoteCard from "./NoteCard";
 import axios from "axios";
 import "../styles/ViewNotes.css";
+import GroupCard from "./GroupCard";
 export default function ViewNotes({ curentUser }) {
-    const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
 
-    useEffect(() => {
-        const FetchData = async () => {
-            try {
-                const resp = await axios.get(
-                    "/api/notes/" + curentUser.id + "/notes"
-                );
-                console.log(resp.data);
-                setNotes(resp.data);
-            } catch (err) {
-                console.error(err);
-            }
-        };
-        FetchData();
-    }, [notes]);
+  useEffect(() => {
+    const FetchData = async () => {
+      try {
+        const resp = await axios.get("/api/notes/" + curentUser.id + "/notes");
 
-    return (
-        <div className="notes-container">
-            {notes.length === 0 && (
-                <h1>You currently haven't added any notes</h1>
-            )}
-            {notes.length > 0 &&
-                notes.map((note) => (
-                    <div key={note.id}>
-                        <NoteCard note={note} curentUser={curentUser} />
-                    </div>
-                ))}
-        </div>
-    );
+        setNotes(resp.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    FetchData();
+  }, [notes]);
+
+  return (
+    <div className="notes-container">
+      {notes.length === 0 && <h1>You currently haven't added any notes</h1>}
+      {notes.length > 0 &&
+        notes.map((note) => (
+          <div key={note.id}>
+            <NoteCard note={note} curentUser={curentUser} />
+          </div>
+        ))}
+    </div>
+  );
 }
